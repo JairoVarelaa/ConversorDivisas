1305b9cb9c19c43988014b670ab564f1d3981d8b
JairoVarelamt <jairo.varela.mt@correo.usa.edu.co>
2023-08-10 10:43:59 -0500

    estructura MVC

---
+++ src/challengeOneBackEnd/Main.java
@@ -0,0 +1,17 @@
+
+package challengeOneBackEnd;
+
+import challengeOneBackEnd.vista.Interfaz;
+
+/**
+ *
+ * @author JairoV
+ */
+public class Main {
+
+    public static void main(String[] args) {
+       Interfaz PantallaPrincipal = new Interfaz();
+       PantallaPrincipal.setVisible(true);
+    }
+    
+}
