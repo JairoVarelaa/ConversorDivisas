eb00986e6e925d8bca420d7f833e4cd8b0ec2eb7
JairoVarelamt <jairo.varela.mt@correo.usa.edu.co>
2023-08-13 16:08:39 -0500

    se cambio el parameto de la hashtable de iconos clase banderas en el modelo por la key Integer

--- src/challengeOneBackEnd/modelo/Banderas.java
+++ src/challengeOneBackEnd/modelo/Banderas.java
@@ -7,15 +7,15 @@ import javax.swing.ImageIcon;
 
 public class Banderas {
    
-    Hashtable<String, ImageIcon> IconoBandera = new Hashtable<>();
+    Hashtable<Integer, ImageIcon> IconoBandera = new Hashtable<>();
     
     void Banderas(){
-        IconoBandera.put("dolar",new ImageIcon(Banderas.class.getResource("img/Dolar.png")));
-        IconoBandera.put("euro",new ImageIcon(Banderas.class.getResource("img/Euros.png")));
-        IconoBandera.put("libraEsterlina",new ImageIcon(Banderas.class.getResource("img/LibraEsterlina.png")));
-        IconoBandera.put("yen",new ImageIcon(Banderas.class.getResource("img/YenJapones.png")));
-        IconoBandera.put("won",new ImageIcon(Banderas.class.getResource("img/WonKoreano.png")));
-        IconoBandera.put("pesoColombiano",new ImageIcon(Banderas.class.getResource("img/PesoColombiano.png")));
+        IconoBandera.put(0,new ImageIcon(Banderas.class.getResource("img/Dolar.png")));
+        IconoBandera.put(1,new ImageIcon(Banderas.class.getResource("img/Euros.png")));
+        IconoBandera.put(2,new ImageIcon(Banderas.class.getResource("img/LibraEsterlina.png")));
+        IconoBandera.put(3,new ImageIcon(Banderas.class.getResource("img/YenJapones.png")));
+        IconoBandera.put(4,new ImageIcon(Banderas.class.getResource("img/WonKoreano.png")));
+        IconoBandera.put(5,new ImageIcon(Banderas.class.getResource("img/PesoColombiano.png")));
     }
     /**
      * Metodo Get para obtener todos los iconos a manera de hashtable
@@ -27,12 +27,12 @@ public class Banderas {
     }
     /**
      * Metodo get para obtener solo 1 valor de la Hashtable a partir de la clabe (key) por parametro.
-     * @param nombreMoneda recibe el valor a consultar en la Hashtable.
+     * @param indice recibe el indice del combobox para consultar en la Hashtable
      * @return El valor de la llave (key).
      */
-    public ImageIcon GetIcono(String nombreMoneda){
+    public ImageIcon GetIcono(int indice){
         
-        return this.IconoBandera.get(nombreMoneda);
+        return this.IconoBandera.get(indice);
     }
     
     
